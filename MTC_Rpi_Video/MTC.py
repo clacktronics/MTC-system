4d1c94129aaf29f694f2895d5650558a42f79bf1
clacktronics <ben@clacktronics.co.uk>
2016-03-07 16:02:12 +0000

    video syncing working better to 0.2s

--- MTC_Rpi_Video/MTC.py
+++ MTC_Rpi_Video/MTC.py
@@ -8,16 +8,19 @@ class MTC():
       self.uart1 = serial.Serial('/dev/ttyAMA0', baudrate=31250)
 
       self.message = [-1] * 8
+      self.saving_clock = False
 
   def saveClock(self):
+      self.saving_clock = True
       self.clock['frames'] = (self.message[1] << 4) + self.message[0] # 2 half bytes 000f ffff
       self.clock['seconds'] = (self.message[3] << 4) + self.message[2] # 2 half bytes 00ss ssss
       self.clock['mins'] =  (self.message[5] << 4) + self.message[4] # 2 half bytes 00mm mmmm
       self.clock['hours'] = ((self.message[7] & 1) << 4) + self.message[6] # 2 half bytes 0rrh hhhh the msb has to be masked as it contains the mode
       self.clock['mode'] = ((self.message[7] & 6) >> 1) # get the fps mode by masking 0rrh with 0110 (6)
+      self.saving_clock = False
 
   def getS(self):
-
+      while self.saving_clock: pass
       mins = ((self.clock['hours'] * 60) + self.clock['mins'])
       seconds = (mins * 60) + self.clock['seconds']
       frames = (seconds * 25) + self.clock['frames']
@@ -38,7 +41,7 @@ class MTC():
 
               if ord(data) == 241:              # if Byte for quater frame message
 
-                  try: mes = int(ord(self.uart1.read(1)))        # Read next byte
+                  try: mes = ord(self.uart1.read(1))        # Read next byte
                   except: continue
 
                   piece = mes >> 4             # Get which part of the message it is (e.g seconds mins)
@@ -56,7 +59,17 @@ class MTC():
 
 
 if __name__ == "__main__":
-	mtc = MTC()
-	while True:
-		print mtc.clock
-		mtc.readFrame()
+
+    import threading
+
+    def get_mtc():
+        while True:
+            mtc.readFrame()
+
+    mtc = MTC()
+    mtc_time = threading.Thread(target=get_mtc)
+    mtc_time.start()
+
+
+    while True:
+        print mtc.clock
--- MTC_Rpi_Video/video.py
+++ MTC_Rpi_Video/video.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     from time import sleep
     import threading
 
-    player = OMXPlayer('timer.mp4')
+    player = OMXPlayer('chem.mp4', args=['--no-osd'])
     mtc = MTC()
     player.play()
 
@@ -22,6 +22,9 @@ if __name__ == "__main__":
 
     sleep(1)
     av = []
+    first_seek = True
+
+    tol = 0.1
 
     try:
       while True:
@@ -30,46 +33,46 @@ if __name__ == "__main__":
         difference = player_pos - mtc_pos
         pl_dev = player_pos - lastp
         mt_dev = mtc_pos - lastm
+        print "(%.2f) %.2f %.2f" % (difference, mtc_pos, player_pos)
 
-        av.append(player_pos)
-        if len(av) > 10:
-            av.pop(0)
-        pl_av = sum(av)/len(av)
+        # av.append(player_pos)
+        # if len(av) > 10:
+        #     av.pop(0)
+        # pl_av = sum(av)/len(av)
+        #
+        # print "Video: %.2f / %.2f (%.2f) MTC: %.2f" % (player_pos, pl_av, pl_dev, mtc_pos)
+        #
+        # # print "%3f %6f" % (pl_dev, mt_dev)
+        # # print sum(av)/len(av)
+        #
+        if difference > 5 or difference < -1:
+            pause_pos = mtc.getS()+5
+            player.set_position(pause_pos)
+            print "seeking to %f" % (pause_pos / 60)
+            player.pause()
+            player_pos = player.position()
+            while mtc.getS() < player_pos: pass
+            player.play()
 
-        print "Video: %.2f / %.2f (%.2f) MTC: %.2f" % (player_pos, pl_av, pl_dev, mtc_pos)
+        # if difference > 0.2:
+        #     player.pause()
+        #     player.play()
 
-        # print "%3f %6f" % (pl_dev, mt_dev)
-        # print sum(av)/len(av)
-
-        if difference > 6 or difference < -6:
-            print difference
-             #player.seek((difference*1000000)*-1)
-
-
-        sleep(0.1)
-
-
-            # pause_pos = mtc.getS()+2
-            # player.set_position(pause_pos)
-            # print "seeking to %f" % (pause_pos / 60)
-            # while mtc.getS() < (pause_pos): pass
-            # player.play()
+        if difference > tol and speed == 0:
+             player.action(1)
+             speed = -1
+        elif difference < -tol and speed == 0:
+             player.action(2)
+             speed = 1
+        elif speed == -1 and difference < tol:
+             player.action(2)
+             speed = 0
+        elif speed == 1 and difference > -tol:
+             player.action(1)
+             speed = 0
 
         lastm = mtc_pos
         lastp = player_pos
-
-        # elif difference > 1 and speed == 0:
-        #     player.action(1)
-        #     speed = -1
-        # elif difference < -1 and speed == 0:
-        #     player.action(2)
-        #     speed = 1
-        # else:
-        #     if speed == 1:
-        #         player.action(1)
-        #     elif speed == -1:
-        #         player.action(2)
-        #     speed = 0
 
 
     except KeyboardInterrupt:
